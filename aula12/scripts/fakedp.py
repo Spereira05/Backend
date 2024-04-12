b5bad492606f5871f8dd1b5dd80c6f0a88936dcc
Santiago Pereira <santiagopereira@MBP-de-Santiago.Home>
2024-04-12 22:49:17 +0100

    aulas 11 a 13

---
+++ aula12/scripts/fakedp.py
@@ -0,0 +1,21 @@
+import json
+from faker import Faker
+
+fake = Faker()
+
+if __name__ == "__main__":
+    total_rows=20
+
+    result = [
+        dict(
+            id=fake.isbn10(),
+            name=" ".join(fake.words(nb=2)),
+            price=fake.pricetag(),
+            enabled=True,
+            sku=100
+        )
+        for _ in range(total_rows)
+    ]
+
+    with open("db.json","w") as file:
+        file.writelines(json.dumps(result, indent=2))
